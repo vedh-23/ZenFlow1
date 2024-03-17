f51e73ab59dde8a19392e66b17d35f9c61e7ece9
vedh-23 <134149515+vedh-23@users.noreply.github.com>
2024-03-17 22:51:35 +0530

    Basicroute

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,17 @@ import React from "react";
 export const Home=()=>{
   return(
     <>
-    Home Page
+    <div className="main">
+      <div className="Hero">
+        <img src="#"></img>
+        <h2></h2>
+      </div>
+      <div className="image">
+        <img src="#"></img>
+      </div>
+
+    </div>
+    
     </>
   )
 }
